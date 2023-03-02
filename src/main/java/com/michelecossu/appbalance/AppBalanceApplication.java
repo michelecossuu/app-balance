cc39fb41b1d15f4df55cd27611e2b9504f7969dc
Michele Cossu <m.cossu4@campus.unimib.it>
2023-03-02 21:35:54 +0100

    first commit: aggiunta progetto

---
+++ src/main/java/com/michelecossu/appbalance/AppBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.michelecossu.appbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelecossu/appbalance/AppBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelecossu.appbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
