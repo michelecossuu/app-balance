32fbc72e807dee91df30961241c52aea68fc4888
Michele Cossu <m.cossu4@campus.unimib.it>
2023-09-21 08:29:24 +0200

    aggiornamento porta BASE_URL a 4200

--- src/main/java/com/michelecossu/appbalance/utils/Costants.java
+++ src/main/java/com/michelecossu/appbalance/utils/Costants.java
@@ -2,6 +2,6 @@ package com.michelecossu.appbalance.utils;
 
 public class Costants {
 	
-	public static final String BASE_URL = "http://localhost:8100";
+	public static final String BASE_URL = "http://localhost:4200";
 	
 }
